88e1cbc34e5f30a2277eeae7f362888e6c95865f
amira-fathalla12 <amira.elwishy09512375321@gmail.com>
2025-01-09 23:27:59 +0200

    'git-test'

--- src/Pages/components/CustomTable/ActionsMenu.tsx
+++ src/Pages/components/CustomTable/ActionsMenu.tsx
@@ -5,11 +5,12 @@ import { DeleteIcon, EditIcon, ViewIcon } from "../SvgIcons/SvgIcons";
 interface Actions {
   openEdit: () => void;
   openDelete: () => void;
-  openview: ()=> void;
-
 }
 
-export default function ActionsMenu({openEdit, openDelete, openview }: Actions) {
+export default function ActionsMenu({
+  openEdit,
+  openDelete,
+}: Actions) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
@@ -20,24 +21,38 @@ export default function ActionsMenu({openEdit, openDelete, openview }: Actions) 
 
       <MenuItems
         transition
-        className="absolute right-0 z-10 mt-2 w-35 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+        className="absolute right-0 z-10 mt-2 w-35 origin-top-right rounded-md
+         bg-white shadow-lg ring-1
+          ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform 
+          data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
       >
         <div className="py-1">
           <MenuItem>
-            <button className="w-full flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none" onClick={openview}>
-              <ViewIcon/>
+            <button
+              className="w-full flex items-center gap-x-2 px-4 py-2 text-sm
+             text-gray-700
+              data-[focus]:bg-gray-100
+               data-[focus]:text-gray-900 data-[focus]:outline-none"
+            >
+              <ViewIcon />
               view
             </button>
           </MenuItem>
           <MenuItem>
-            <button className="w-full flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none" onClick={openEdit}>
-              <EditIcon/>
+            <button
+              className="w-full flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+              onClick={openEdit}
+            >
+              <EditIcon />
               Edit
             </button>
           </MenuItem>
           <MenuItem>
-            <button className="w-full flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none" onClick={openDelete}>
-              <DeleteIcon/>
+            <button
+              className="w-full flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+              onClick={openDelete}
+            >
+              <DeleteIcon />
               Delete
             </button>
           </MenuItem>
